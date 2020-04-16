a2c8182f08c8800f920e94188ebf24f45ef8cc3a
biji <heapoverflow@kakao.com>
2020-04-17 02:35:21 +0900

    Update calc.c
    Add caption

--- calc.c
+++ calc.c
@@ -4,11 +4,11 @@
 #include "function.h"
 
 int main(){
-    char exp[100], tmp[100];
-    unsigned int idx_stack = 0;
-    unsigned int idx_tmp = 0;
+    char exp[100], tmp[100]; // 입력받은 수식 exp, 후위표기법으로 변환한 수식 tmp
+    unsigned int idx_stack = 0; // stack 인덱스
+    unsigned int idx_tmp = 0; // 후위표기법 변환 시 사용할 인덱스
 
-    memset(exp,0,100);
+    memset(exp,0,100); // 변수 초기화
     memset(tmp,0,100);
 
     printf(">> Input : ");
@@ -21,15 +21,15 @@ int main(){
             index++;
         }
     }
-    exp[index] = 0;
+    exp[index] = 0; // 수식 마지막 NULL 문자 추가 - 문자열의 끝
         
-    printf("[*] Infix Notation : %s\n", exp);
+    printf("[*] Infix Notation : %s\n", exp); // 공백 없앤 중위 표기법 형식 출력
+ 
+    infix_to_postfix(exp,tmp,index,idx_stack,idx_tmp); // 중위 표기법 -> 후위 표기법 변환
 
-    infix_to_postfix(exp,tmp,index,idx_stack,idx_tmp);
+    printf("[+] Postfix Notation : %s\n", tmp); // 후위 표기법 출력
 
-    printf("[+] Postfix Notation : %s\n", tmp);
+    int ret = evalPostfix(tmp); // 후위 표기법으로 수식 연산
 
-    int ret = evalPostfix(tmp);
-
-    printf("[+] answer >> %d\n", ret);
+    printf("[+] answer >> %d\n", ret); // 연산 결과 출력
 }
