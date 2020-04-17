8e281f51020cca526afe791282f4de6ff54943b9
biji <heapoverflow@kakao.com>
2020-04-17 10:04:46 +0900

    Update calc.c
    Add error control function

--- calc.c
+++ calc.c
@@ -22,6 +22,23 @@ int main(){
         }
     }
     exp[index] = 0; // 수식 마지막 NULL 문자 추가 - 문자열의 끝
+    
+        bool is_error = false; // 에러 처리를 위한 bool 변수. true면 에러가 존재하는 수식임.
+    for(int i = 1; i<100; i++){
+        // 연산자가 두개 연속으로 나올 경우 에러
+        if((exp[i]=='+'||exp[i]=='-'||exp[i]=='/'||exp[i]=='*') && (exp[i-1]=='+'||exp[i-1]=='-'||exp[i-1]=='/'||exp[i-1]=='*'))
+            is_error = true;
+        if(exp[i]=='0' && exp[i-1]=='/') // 0으로 나누는 경우 에러
+            is_error = true;
+        // 수식의 마지막에 연산자가 나올 경우 에러.
+        if(exp[strlen(exp)-2]=='*'||exp[strlen(exp)-2]=='/'||exp[strlen(exp)-2]=='+'||exp[strlen(exp)-2]=='-')
+            is_error = true;
+    }
+    
+    if(is_error == true){
+        printf("[-] Error in formula\n");
+        return 0;
+    }
         
     printf("[*] Infix Notation : %s\n", exp); // 공백 없앤 중위 표기법 형식 출력
  
